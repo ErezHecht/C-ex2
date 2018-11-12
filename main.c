54c31ded9fdb61aa809acc15bd11eecee9c4d6e9
erezuni <erez.hecht@mail.huji.ac.il>
2018-11-12 21:27:56 +0200

    MANY MANY FIXES

--- main.c
+++ main.c
@@ -46,8 +46,10 @@
 #define S_LOC 3
 #define G_LOC 4
 #define NUM_OF_ARGS 5
-
+#define MALLOC_ERROR "Error allocating memory."
+#define REALLOC_ERROR "Error re-allocating memory."
 #define MAX(x, y) ((x)>(y)?(x):(y))
+
 
 /**
  * TODO:
@@ -81,18 +83,25 @@ Sequence *initializeSequence()
     Sequence *seq = (Sequence *) malloc(sizeof(Sequence));
     if (seq == NULL)
     {
-        fprintf(stderr, "Error Mallocating");
+        fprintf(stderr, MALLOC_ERROR);
         exit(EXIT_FAILURE);
     }
     seq->name = (char *) malloc(sizeof(char));
     seq->sequence = (char *) malloc(sizeof(char));
+    seq->sequence[0] = '\0';
     return seq;
 }
 
+void freeSequence(Sequence sequence)
+{
+    free(sequence.sequence);
+    free(sequence.name);
+//    free(sequence);
+}
 
 typedef struct TableCell
 {
-    int score;
+    size_t score;
     struct TableCell *previous;
 } TableCell;
 
@@ -108,24 +117,93 @@ TableCell *initTableCell()
     tc->previous = NULL;
     return tc;
 }
+
+void freeCell(TableCell tableCell)
+{
+    if (tableCell.previous != NULL)
+    {
+        free(tableCell.previous);
+    }
+//    free(tableCell);
+}
+
+//-------------------------------Globals-------------------------------
+Sequence sequences[MAX_SEQUENCES];
+size_t numberOfSequences = 0;
+
+/**
+ * @brief Free the memory of the sequences.
+ *
+ * @param numberOfSequences the number of sequences to free.
+ * */
+void freeSequences()
+{
+    for (int i = 0; i < numberOfSequences; ++i)
+    {
+        freeSequence(sequences[i]);
+    }
+}
+//-------------------------------Utility Functions-------------------------------
+
+void *allocateMemory(size_t num, size_t size)
+{
+    void *out = malloc(num * size);
+    if (out == NULL)
+    {
+        freeSequences();
+        fprintf(stderr, MALLOC_ERROR);
+        exit(EXIT_FAILURE);
+    }
+    return out;
+}
+
+void *reallocateMemory(void *src, size_t num, size_t size)
+{
+    void *out = realloc(src, num * size);
+    if (out == NULL)
+    {
+        freeSequences();
+        fprintf(stderr, REALLOC_ERROR);
+        exit(EXIT_FAILURE);
+    }
+    return out;
+}
+
 //-------------------------------Read File-------------------------------//
+/**
+ * @brief Get rid of unwanted characters in the given line, and return the new size of the line.
+ *
+ * @param line - the line to change
+ * @param lineLen - the length of the line
+ * */
+size_t getRidOfNewline(char *line, size_t lineLen)
+{
+    size_t newLen = lineLen;
+    if (lineLen > 0 && line[lineLen - 1] == '\n')
+    {
+        line[lineLen - 1] = '\0';
+        newLen--;
+    }
+    if (lineLen > 1 && line[newLen - 1] == '\r')
+    {
+        line[lineLen - 2] = '\0';
+        newLen--;
+    }
+    return newLen;
+}
+
 /**
  * @brief sets the name parameter of the given Sequence.
  * */
 void setName(Sequence *seq, char *line, size_t lineLen)
 {
-
-    seq->name = (char *) realloc(seq->name, lineLen * sizeof(char));
-    if (seq->name == NULL)
-    {
-        fprintf(stderr, "Error Mallocating");
-        exit(EXIT_FAILURE);
-    }
-
+//    seq->name = (char *) realloc(seq->name, lineLen * sizeof(char));
+    seq->name = (char *) reallocateMemory(seq->name, lineLen, sizeof(char));
+    //-1 for the ">" at the start.
     strncpy(seq->name, &line[1], lineLen - 1);
 }
 
-unsigned int readFile(const char *address, Sequence *sequences)
+size_t readFile(const char *address)
 {
     FILE *f = fopen(address, "r");
     if (f == NULL)
@@ -134,20 +212,13 @@ unsigned int readFile(const char *address, Sequence *sequences)
         exit(EXIT_FAILURE);
     }
     char line[MAX_LINE_LEN];
-    unsigned int numOfSequences = 0;
+    size_t numOfSequences = 0;
     Sequence *curr = NULL;
     while (fgets(line, MAX_LINE_LEN, f) && numOfSequences < MAX_SEQUENCES)
     {
-        //Next few lines are parts of sequence
-        size_t lineLen = strlen(line);
-        if (lineLen > 0 && line[lineLen - 1] == '\n')
-        {
-            line[lineLen - 1] = '\0';
-            if (line[lineLen - 2] == '\r')
-            {
-                line[lineLen - 2] = '\0';
-            }
-        }
+        //Get the length of the line after we get rid of \r and \n in it:
+        size_t lineLen = getRidOfNewline(line, strlen(line));
+        //Case: Line is the start of a new Sequence segment:
         if (line[0] == '>')
         {
             curr = initializeSequence();
@@ -156,7 +227,10 @@ unsigned int readFile(const char *address, Sequence *sequences)
             numOfSequences++;
         } else
         {
-            curr->sequence = (char *) realloc(curr->sequence, (strlen(curr->sequence) + lineLen) * sizeof(char)-1);
+//            curr->sequence = (char *) realloc(curr->sequence, (strlen(curr->sequence) + lineLen) * sizeof(char) + 1);
+            curr->sequence = (char *) reallocateMemory(curr->sequence, (strlen(curr->sequence) + lineLen) + 1,
+                                                       sizeof(char));
+
             strcat(curr->sequence, line);
         }
     }
@@ -174,122 +248,84 @@ unsigned int readFile(const char *address, Sequence *sequences)
  * 3. Traverse back from Table[n][m] to get the actual best match.
  * */
 
-TableCell *getTableCell(TableCell *table, const int row, const int col, const int cols)
+
+TableCell *getTableCell(TableCell *table, const size_t row, const size_t col, const size_t cols)
 {
-    return (table + (row * cols + col));
+    return table + (row * cols + col);
 }
 
-void initTable(TableCell *table, const int rows, const int cols, const int g)
+/**
+ * @brief initializes the first row and column of a given table.
+ * */
+void initTable(TableCell *table, const size_t rows, const size_t cols, const int g)
 {
-    for (int i = 0; i < rows; ++i)
+    //Initialize the first column:
+    for (size_t i = 0; i < rows; ++i)
     {
         getTableCell(table, i, 0, cols)->score = i * g;
     }
     //Initialize the first row:
-    for (int j = 0; j < cols; ++j)
+    for (size_t j = 0; j < cols; ++j)
     {
         getTableCell(table, 0, j, cols)->score = j * g;
     }
 
 }
 
-//void
-//setMax(TableCell *table, const int i, const int j, const size_t cols, const int upLeft, const int up, const int left)
-//{
-//    TableCell *max = NULL;
-//    int score = 0;
-//    if (upLeft > up)
-//    {
-//        if (upLeft > left)
-//        {
-//            max = (table + (i - 1) * cols + (j - 1));
-//            score = upLeft;
-//        } else
-//        {
-//            max = (table + (i) * cols + (j - 1));
-//            score = left;
-//        }
-//    } else
-//    {
-//        if (left > up)
-//        {
-//            max = (table + (i) * cols + (j - 1));
-//            score = left;
-//        } else
-//        {
-//            max = (table + (i - 1) * cols + (j));
-//            score = up;
-//        }
-//    }
-//    (table + i * cols + j)->previous = max;
-//    (table + i * cols)->score = score;
-//}
-void printHelper(const Sequence *seq1, const Sequence *seq2, TableCell *firstCell, int n, int m)
-{
-
-    for (int j = -1; j <= m; ++j)
-    {
-        for (int i = -1; i <= n; ++i)
-        {
-            if (i == -1 && j == -1)
-            {
-                printf("   ");
-            } else if (i == -1)
-            {
-                printf("  %c ", seq2->sequence[j - 1]);
-            } else if (j == -1)
-            {
-                printf("  %c ", seq1->sequence[i - 1]);
-            } else
-            {
-                printf(getTableCell(firstCell, j, i, n)->score < 0 ? "%.2d " : " %.2d ",
-                       getTableCell(firstCell, j, i, n)->score);
-            }
-
-        }
-        printf("\n");
-    }
-}
-
 /**
  * @brief Compares two sequences to find the longest match between them.
+ *
+ * @param seq1 the first sequence to compare
+ * @param seq2 the second sequence to compare
+ * @param m the weight of a match
+ * @param s the weight of a miss-match
+ * @param g the weight of a gap
+ *
+ * @return the length of the longest match
  * */
-int compareSequences(const Sequence *seq1, const Sequence *seq2, const int m, const int s, const int g)
+void compareSequences(const Sequence *seq1, const Sequence *seq2, const int m, const int s, const int g)
+{
+    // +1 for the extra rows and cols
+    size_t rows = strlen(seq1->sequence) + 1;
+    size_t cols = strlen(seq2->sequence) + 1;
+//    TableCell *table = (TableCell *) malloc(rows * cols * sizeof(TableCell));
+    TableCell *table = (TableCell *) allocateMemory(rows * cols, sizeof(TableCell));
+    initTable(table, rows, cols, g);
+    //For each cell of the table, from top left to bottom right, calculate the value.
+    for (size_t i = 1; i < rows; ++i)
+    {
+        for (size_t j = 1; j < cols; ++j)
+        {
+            size_t upLeft = getTableCell(table, i - 1, j - 1, cols)->score;
+            upLeft += (seq1->sequence[i - 1] == seq2->sequence[j - 1]) ? m : s;
+            size_t left = getTableCell(table, i, j - 1, cols)->score + g;
+            size_t up = getTableCell(table, i - 1, j, cols)->score + g;
+            getTableCell(table, i, j, cols)->score = MAX(MAX(upLeft, up), left);
+        }
+    }
+    printf("%li", getTableCell(table, (rows - 1), (cols - 1), cols)->score);
+    //free the memory:
+    for (size_t k = 0; k < rows; ++k)
+    {
+        for (size_t j = 0; j < cols; ++j)
+        {
+//            printf("%d ", getTableCell(table, k, j, cols)->score);
+            freeCell(*getTableCell(table, k, j, cols));
+        }
+//        printf("\n");
+    }
+    free(table);
+}
+
+int *getCell(int *table, const int row, const int col, const int cols)
+{
+    return table + (row * cols + col);
+}
+
+void compareSeq(Sequence *seq1, Sequence *seq2, int m, int s, int g)
 {
     int rows = (int) strlen(seq1->sequence) + 1;
     int cols = (int) strlen(seq2->sequence) + 1;
-    TableCell *table = (TableCell *) malloc(rows * cols * sizeof(TableCell));
-    initTable(table, rows, cols, g);
-    for (int i = 1; i < rows; ++i)
-    {
-        for (int j = 1; j < cols; ++j)
-        {
-            int upLeft = getTableCell(table, i - 1, j - 1, cols)->score;
-            upLeft += (seq1->sequence[i] == seq2->sequence[j]) ? m : s;
-            int left = getTableCell(table, i, j - 1, cols)->score + g;
-            int up = getTableCell(table, i - 1, j, cols)->score + g;
-            getTableCell(table, i, j, cols)->score = MAX(MAX(upLeft, up), left);
-//            setMax(table, i, j, cols, upLeft, up, left);
-        }
-    }
-    printHelper(seq1, seq2, table, rows, cols);
-//    for (int k = 0; k < rows; ++k)
-//    {
-//        for (int i = 0; i < cols; ++i)
-//        {
-//            printf("%d ", getTableCell(table, k, i, cols)->score);
-//        }
-//        printf("\n");
-//    }
-    return getTableCell(table, (rows-1), cols, cols)->score;
-
-}
-
-//V2:
-int compareSeq(Sequence *seq1, Sequence *seq2, int m, int s, int g)
-{
-    unsigned long int rows = strlen(seq1->sequence) + 1;
-    unsigned long int cols = strlen(seq2->sequence) + 1;
     int *table = (int *) malloc(rows * cols * sizeof(int));
     if (table == NULL)
     {
@@ -298,38 +334,80 @@ int compareSeq(Sequence *seq1, Sequence *seq2, int m, int s, int g)
     }
     for (int i = 0; i < rows; ++i)
     {
-        *(table + (i * cols)) = i * g;
+        *getCell(table, i, 0, cols) = i * g;
     }
     for (int i = 0; i < cols; ++i)
     {
-        *(table + i) = i * g;
+        *getCell(table, 0, i, cols) = i * g;
     }
     for (int i = 1; i < rows; ++i)
     {
         for (int j = 1; j < cols; ++j)
         {
-            int upleft = *(table + (i - 1) * cols + (j - 1));
-            int left = *(table + (i) * cols + (j - 1) + g);
-            int up = *(table + (i - 1) * cols + j) + g;
-            if (seq1->sequence[i] == seq2->sequence[j])
-            {
-                upleft += m;
-            } else
-            {
-                upleft += s;
-            }
-            *(table + (i * cols) + j) = (int) fmax(fmax(upleft, left), up);
+            int upleft = *getCell(table, (i - 1), (j - 1), cols);
+            int left = *getCell(table, i, (j - 1), cols) + g;
+            int up = *getCell(table, (i - 1), j, cols) + g;
+            upleft += (seq1->sequence[i - 1] == seq2->sequence[j - 1]) ? m : s;
+            *getCell(table, i, j, cols) = MAX(MAX(upleft, left), up);
         }
     }
     for (int k = 0; k < rows; ++k)
     {
         for (int i = 0; i < cols; ++i)
         {
-            printf("%d ", *(table + k * cols + i));
+            printf("%i ", *getCell(table, (k), (i), cols));
         }
         printf("\n");
     }
-    return *(table + rows * cols + cols);
+    printf("Score for alignment of sequence %s to sequence %s is %i\n", seq1->name, seq2->name,
+           *getCell(table, (rows - 1), (cols - 1), cols));
+    free(table);
+}
+
+
+//V3: WORKS!!!!
+int compareSeq2(Sequence *seq1, Sequence *seq2, int m, int s, int g)
+{
+    unsigned long int rows = strlen(seq1->sequence) + 1;
+    unsigned long int cols = strlen(seq2->sequence) + 1;
+    int **table = (int **) malloc(rows * sizeof(int *));
+    for (int i = 0; i < rows; i++)
+    {
+        table[i] = (int *) malloc(cols * sizeof(int));
+    }
+    if (table == NULL)
+    {
+        printf("ERRRORROROROOROR");
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 0; i < rows; ++i)
+    {
+        table[i][0] = i * g;
+    }
+    for (int i = 0; i < cols; ++i)
+    {
+        table[0][i] = i * g;
+    }
+    for (int i = 1; i < rows; ++i)
+    {
+        for (int j = 1; j < cols; ++j)
+        {
+            int upleft = table[i - 1][j - 1];
+            int left = table[i][j - 1] + g;
+            int up = table[i - 1][j] + g;
+            upleft += (seq1->sequence[i - 1] == seq2->sequence[j - 1]) ? m : s;
+            table[i][j] = (int) fmax(fmax(upleft, left), up);
+        }
+    }
+    for (int k = 0; k < rows; ++k)
+    {
+        for (int i = 0; i < cols; ++i)
+        {
+            printf("%d ", table[k][i]);
+        }
+        printf("\n");
+    }
+    return table[rows - 1][cols - 1];
 }
 
 //-------------------------------Main-------------------------------//
@@ -340,8 +418,6 @@ void validateArgs(int argc)
         printf("Usage: text.txt m s g");
         exit(EXIT_FAILURE);
     }
-
-
 }
 
 int getWeight(const char *src)
@@ -360,25 +436,30 @@ int getWeight(const char *src)
 int main(int argc, char *argv[])
 {
     validateArgs(argc);
-    Sequence *sequences = (Sequence *) malloc(sizeof(Sequence) * MAX_SEQUENCES);
-    unsigned int numOfSequences = readFile(argv[1], sequences);
-    sequences = (Sequence *) realloc(sequences, numOfSequences * sizeof(Sequence));
-    for (int k = 0; k < numOfSequences; ++k)
+
+    numberOfSequences = readFile(argv[1]);
+    for (int k = 0; k < numberOfSequences; ++k)
     {
         printf("%s\n", sequences[k].sequence);
     }
     int m = getWeight(argv[M_LOC]);
     int s = getWeight(argv[S_LOC]);
     int g = getWeight(argv[G_LOC]);
-    for (int i = 0; i < numOfSequences; ++i)
+    for (int i = 0; i < numberOfSequences; ++i)
     {
-        for (int j = i + 1; j < numOfSequences; ++j)
+        for (int j = i + 1; j < numberOfSequences; ++j)
         {
-            printf("%d\n", compareSequences(&sequences[i], &sequences[j], m, s, g));
-            //printf("%d\n", compareSeq(&sequences[i], &sequences[j], m, s, g));
+//            compareSequences(&sequences[i], &sequences[j], m, s, g);
+//            printf("\n-----------------------------------------------------------------\n");
+            compareSeq(&sequences[i], &sequences[j], m, s, g);
+            printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+//            printf("%d\n", compareSeq2(&sequences[i], &sequences[j], m, s, g));
+//            printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
 
         }
     }
+    freeSequences();
 
 
 }
